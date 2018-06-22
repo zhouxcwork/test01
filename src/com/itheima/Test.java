9f94b88fcefa56d5e63f1ea4c0c9692ca434f77b
zhouxcwork <1966996700@qq.com>
2018-06-22 21:37:12 +0800

    init

---
+++ src/com/itheima/Test.java
@@ -0,0 +1,22 @@
+package com.itheima;
+
+/**
+ * Created by zxc on 2018/6/22.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = a + 1;
+        System.out.println("hello world");
+        System.out.println("hello world");
+        test();
+        System.out.println("hello world");
+    }
+
+    public static void test() {
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+        System.out.println("aa");
+    }
+}
