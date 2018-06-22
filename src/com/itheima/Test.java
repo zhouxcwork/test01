ce15007d2e41e4b4f97d7a2ab95835f29c843e2e
zhouxcwork <1966996700@qq.com>
2018-06-22 21:44:24 +0800

    init

--- src/com/itheima/Test.java
+++ src/com/itheima/Test.java
@@ -10,6 +10,7 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hello world");
         test();
+        System.out.println("sd");
         System.out.println("hello world");
     }
 
